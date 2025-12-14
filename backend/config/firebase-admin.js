ed7d45ed89af72e001cd49e31121c2eeaab038aa
NguyenDat004 <nguyendat135ok@gmail.com>
2025-12-14 22:26:37 +0700

    Deploy update

--- backend/config/firebase-admin.js
+++ backend/config/firebase-admin.js
@@ -1,5 +1,11 @@
 const admin = require('firebase-admin');
-const serviceAccount = require('./firebaseServiceAccountKey.json');
+const serviceAccount = {
+  type: "service_account",
+  project_id: process.env.FIREBASE_PROJECT_ID,
+  private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+  client_email: process.env.FIREBASE_CLIENT_EMAIL,
+};
+
 
 // Tránh lỗi khi khởi tạo lại app nhiều lần
 if (!admin.apps.length) {
