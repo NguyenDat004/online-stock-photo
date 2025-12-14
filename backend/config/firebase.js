ce166c5db70c24a54002b7ff6a32d0ee60173fe0
NguyenDat004 <nguyendat135ok@gmail.com>
2025-12-14 22:17:24 +0700

    Deploy update2

--- backend/config/firebase.js
+++ backend/config/firebase.js
@@ -1,8 +1,15 @@
 const admin = require('firebase-admin');
-const serviceAccount = require('./firebaseServiceAccountKey.json');
+
+const serviceAccount = {
+  type: "service_account",
+  project_id: process.env.FIREBASE_PROJECT_ID,
+  private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+  client_email: process.env.FIREBASE_CLIENT_EMAIL,
+};
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
 });
 
+console.log('✅ Firebase Admin initialized');
 module.exports = admin;
